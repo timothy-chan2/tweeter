fea5e8585a79e174e642cda2d1744fed2d3574ae
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-16 18:59:01 +0000

    Add jQuery to count the available characters leftfrom 140  as user is typing

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+// Ensure the DOM has loaded
+$(document).ready(function(){
+  console.log('DOM is ready!');
+
+  // // Reduce characters left when typing
+  $("#tweet-text").keypress(function(){
+    const count = 139 - $(this).val().length;
+    $(this).next().children(".counter").text(count);
+    if (count < 0) {
+      $(this).next().children(".counter").addClass("exceed");
+    }
+  });
+
+  // Add characters left when pressing backspace
+  $("#tweet-text").keydown(function(event){
+    if (event.originalEvent.key === "Backspace") {
+      if (141 - $(this).val().length <= 140) {
+        const count = 141 - $(this).val().length;
+        $(this).next().children(".counter").text(count);
+        if (count > -1) {
+          $(this).next().children(".counter").removeClass("exceed");
+        }
+      }
+    }
+  });
+});
