94125eefc4667f22c1149a86853bb2df7836d058
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-16 23:22:23 +0000

    Add empty space before { of functions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 // Ensure the DOM has loaded
-$(document).ready(function(){
+$(document).ready(function() {
   console.log('DOM is ready!');
 
   // // Reduce characters left when typing
-  $("#tweet-text").keypress(function(){
+  $("#tweet-text").keypress(function() {
     const count = 139 - $(this).val().length;
     $(this).next().children(".counter").text(count);
     if (count < 0) {
@@ -14,7 +14,7 @@ $(document).ready(function(){
   });
 
   // Add characters left when pressing backspace
-  $("#tweet-text").keydown(function(event){
+  $("#tweet-text").keydown(function(event) {
     if (event.originalEvent.key === "Backspace") {
       if (141 - $(this).val().length <= 140) {
         const count = 141 - $(this).val().length;
