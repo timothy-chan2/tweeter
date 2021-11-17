eb975a23c23c00876c95014aebc7f3d6566b268c
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-17 22:37:01 +0000

    Remove hard-coded tweets & add function to get the tweets from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const createTweetElement = (tweet) => {
     return `
       <article class="single-tweet">
@@ -62,7 +37,20 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: function(data) {
+        renderTweets(data);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  };
+
+  loadTweets();
 
   $('form').submit((event) => {
     event.preventDefault(); // To prevent the default form submission behaviour
@@ -82,4 +70,5 @@ $(document).ready(function() {
       }
     });
   });
+
 });
