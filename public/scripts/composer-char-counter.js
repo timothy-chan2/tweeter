8f24be64e8d599a558ce3e4b73b59fd6794a2d9c
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-17 01:54:02 +0000

    Refactored the code to be shorter by using keyup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,26 +3,13 @@ $(document).ready(function() {
   console.log('DOM is ready!');
 
   // // Reduce characters left when typing
-  $("#tweet-text").keypress(function() {
-    const count = 139 - $(this).val().length;
+  $("#tweet-text").keyup(function() {
+    const count = 140 - $(this).val().length;
     $(this).next().children(".counter").text(count);
     if (count < 0) {
       $(this).next().children(".counter").addClass("exceed");
     } else {
       $(this).next().children(".counter").removeClass("exceed");
-    }
-  });
-
-  // Add characters left when pressing backspace
-  $("#tweet-text").keydown(function(event) {
-    if (event.originalEvent.key === "Backspace") {
-      if (141 - $(this).val().length <= 140) {
-        const count = 141 - $(this).val().length;
-        $(this).next().children(".counter").text(count);
-        if (count > -1) {
-          $(this).next().children(".counter").removeClass("exceed");
-        }
-      }
     }
   });
 });
